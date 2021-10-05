79740c0034c01aa4df66995d0d13a9d4ae502f51
ykn113 <yukinosnj13@gmail.com>
2021-10-05 16:15:13 +0900

    [Add]animations to User clicks

--- game.js
+++ game.js
@@ -1,38 +1,34 @@
-var gamePattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+var userClickedPattern = [];
 
-function nextSequence(){
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  animatePress(userChosenColour);
+  playSound(userChosenColour);
+});
+
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-// var green_audio = new Audio('sounds/green.mp3');
-// $("#green").click(function(){
-//   $("#green").fadeOut(100).fadeIn(100);
-//   green_audio.play();
-// })
-//
-// var red_audio = new Audio('sounds/red.mp3');
-// $("#red").click(function(){
-//   $("#red").fadeOut(100).fadeIn(100);
-//   red_audio.play();
-// })
-//
-// var yellow_audio = new Audio('sounds/yellow.mp3');
-// $("#yellow").click(function(){
-//   $("#yellow").fadeOut(100).fadeIn(100);
-//   yellow_audio.play();
-// })
-//
-// var blue_audio = new Audio('sounds/blue.mp3');
-// $("#blue").click(function(){
-//   $("#blue").fadeOut(100).fadeIn(100);
-//   blue_audio.play();
-// })
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
